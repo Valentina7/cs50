ecc72ed893655278e59f22fd95f1ae1489148dec
Valentina Grushetska <grushetskay@gmail.com>
2018-12-08 09:07:33 +0000

    Fixed code formating.

--- coin.c
+++ coin.c
@@ -5,37 +5,40 @@ int main(void)
 {
     float change1;
 
- do
+    do
     {
         printf ("Укажите размер сдачи:\n");
         change1 = get_float ();
     }
-while (change1 < 0);
+    while (change1 < 0);
 
     int change2 = (change1*100);
 
-        int coin = 0;
+    int coin = 0;
 
     while (change2 >= 25)
-        {
-            change2 = change2-25;
-            coin++;
-        }
-    while (change2 >= 10)
-        {
-            change2= change2-10;
-            coin++;
-        }
-    while (change2 >= 5)
-        {
-            change2 = change2-5;
-            coin++;
-        }
-    while (change2 >= 1)
-        {
-            change2 = change2-1;
-            coin++;
-        }
+    {
+        change2 = change2-25;
+        coin++;
+    }
 
-        printf("%i\n", coin);
+    while (change2 >= 10)
+    {
+        change2= change2-10;
+        coin++;
+    }
+
+    while (change2 >= 5)
+    {
+        change2 = change2-5;
+        coin++;
+    }
+
+    while (change2 >= 1)
+    {
+        change2 = change2-1;
+        coin++;
+    }
+
+    printf("%i\n", coin);
 }
